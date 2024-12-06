faa848dae92d13d7a86d9b12f0f669a4ebd8ebab
aoshisen <aoshisen.23@gmail.com>
2024-12-06 11:57:29 +0800

    refactor: 优化代码逻辑

--- src/plum.ts
+++ src/plum.ts
@@ -19,78 +19,69 @@ export class Plum {
     this.ctx = el.getContext("2d")!;
     this.init();
   }
+
   private init(): void {
     this.ctx.strokeStyle = "grey";
-    const branch: Branch = {
+    const branch: Branch = Object.freeze({
       start: { x: this.el.width / 2, y: this.el.height },
-      length: this.getRandomLength(),
+      length: this.randomLength(),
       theta: -Math.PI / 2,
-    };
+    });
     this.step(branch);
     this.startAnimation();
   }
   private startAnimation(): void {
+    if (this.shouldYield()) return;
     const currentFrames = [...this.frameQueue];
     this.frameQueue = [];
     currentFrames.forEach((frame) => frame());
-    if (currentFrames.length && this.depth < this.MAX_DEPTH) {
-      requestAnimationFrame(() => this.startAnimation());
-      this.depth++;
-    }
+    requestAnimationFrame(() => this.startAnimation());
+    this.depth++;
   }
-  private getNextPoint(b: Branch): Point {
-    return {
+
+  private nextPoint(b: Branch): Point {
+    return Object.freeze({
       x: b.start.x + Math.cos(b.theta) * b.length,
       y: b.start.y + Math.sin(b.theta) * b.length,
-    };
+    })
   }
-  private lineTo(s: Point, e: Point): void {
+
+  private shouldYield(): boolean {
+    return this.frameQueue.length === 0 || this.depth >= this.MAX_DEPTH
+  }
+
+  private line(s: Point, e: Point): Point {
     this.ctx.beginPath();
     this.ctx.moveTo(s.x, s.y);
     this.ctx.lineTo(e.x, e.y);
     this.ctx.stroke();
+    return e;
   }
   private branch(b: Branch): Point {
-    const endPoint = this.getNextPoint(b);
-    this.lineTo(b.start, endPoint);
-    return endPoint;
+    return this.line(b.start, this.nextPoint(b));
   }
-  private getRandom(): number {
+  private random(): number {
     return Math.random();
   }
-  private getRandomLength(): number {
+  private randomLength(): number {
     // 5-15
     return Math.random() * 10 + 5;
   }
-  private getNextLeftBranch(b: Branch, end: Point): Branch {
-    return {
-      theta: b.theta - this.getRandom() * 0.2,
+  private getNextBranch(b: Branch, end: Point, direction: "left" | "right"): Branch {
+    const symbol = direction === "left" ? -1 : 1;
+    return Object.freeze({
+      theta: b.theta + (symbol * this.random() * 0.2),
       start: end,
-      length: this.getRandomLength(),
-    };
-  }
-  private getNextRightBranch(b: Branch, end: Point): Branch {
-    return {
-      theta: b.theta + this.getRandom() * 0.2,
-      start: end,
-      length: this.getRandomLength(),
-    };
+      length: this.randomLength(),
+    })
   }
   private checkIsRenderBranch() {
-    return this.depth < this.MIN_DEPTH || this.getRandom() < 0.5;
+    return this.depth < this.MIN_DEPTH || this.random() < 0.5;
   }
   private step(b: Branch): void {
+    if (!this.checkIsRenderBranch()) return;
     const endPoint = this.branch(b);
-    if (this.checkIsRenderBranch()) {
-      this.frameQueue.push(() =>
-        this.step(this.getNextLeftBranch(b, endPoint))
-      );
-    }
-
-    if (this.checkIsRenderBranch()) {
-      this.frameQueue.push(() =>
-        this.step(this.getNextRightBranch(b, endPoint))
-      );
-    }
+    this.frameQueue.push(() => this.step(this.getNextBranch(b, endPoint, "left")));
+    this.frameQueue.push(() => this.step(this.getNextBranch(b, endPoint, "right")));
   }
 }
